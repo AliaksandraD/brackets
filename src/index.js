d68dbbff95cf2cf8371dade70901128208188a49
aliaksandrad <avetta19770@gmail.com>
2023-02-21 23:39:29 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length %2 != 0) {
+    return false;
+  }
+let e = [];
+let br = str.split('');
+  for (let i = 0; i<str.length; i++){
+    for (let p = 0; p<bracketsConfig.length; p++){
+      if (br[i] == bracketsConfig[p][1] && (e[e.length-1] == bracketsConfig[p][0]))  {
+        e.pop();
+      } else  if (br[i] == bracketsConfig[p][0]) {
+        e.push(br[i]);
+      }
+      
+
+    }
+  }
+  return e.length === 0;
 }
